e555e374eafedfcc385ec694d07247c0ba269824
undrfined <undrfined@gmail.com>
2019-03-03 18:01:31 +0200

    fixed performance for diffing (needs testing)
    - added cool ui
    - added preview html

--- ivdiff.py
+++ ivdiff.py
@@ -8,12 +8,17 @@ import webbrowser
 import os
 import argparse
 from urllib.parse import urlparse
-from urllib.parse import urlencode
-from urllib.parse import urljoin
 from http.cookies import SimpleCookie
 from hashlib import md5
+import time
+import copy
+import urllib3
 
 logging.basicConfig(filename="ivdiff.log", level=logging.INFO)
+htmlparser = etree.HTMLParser(remove_blank_text=True)
+verify = True
+if not verify:
+    urllib3.disable_warnings()
 
 
 def getHtml(domain, cookies, url, template):
@@ -32,79 +37,108 @@ def getHtml(domain, cookies, url, template):
         d = "https://instantview.telegram.org/{}/{}".format(contest, domain)
     else:
         d = "https://instantview.telegram.org/{}/{}/template{}".format(contest, domain, templNumber)
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": d,
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Cache-Control": "max-age=0"
+    }
     logging.info("-- Getting html for {} --".format(url.encode("ascii")))
-    r = requests.get(d, cookies=cookies, params=dict(url=url))
+    # print("-- Getting html for {} --".format(url.encode("ascii")))
+
+    r = requests.get(d, headers=headers, verify=verify, cookies=cookies, params=dict(url=url))
     cookies = dict(list(cookies.items()) + list(r.cookies.get_dict().items()))
 
     hash = re.search("{}\\?hash=(.*?)\",".format(contest), str(r.content)).group(1)
-    logging.info("hash={}".format(hash))
+    # logging.info("hash={}".format(hash))
+    # print(f"got hash {hash}")
 
     rules = rules.encode('utf-8')
-
-    r = requests.post("https://instantview.telegram.org/api/{}".format(contest), cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=""))
+    headers["X-Requested-With"] = "XMLHttpRequest"
+    headers["Accept"] = "application/json, text/javascript, */*; q=0.01"
+    r = requests.post("https://instantview.telegram.org/api/{}".format(contest), headers=headers, verify=verify, cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=""))
     random_id = r.json()["random_id"]
-    logging.info("random_id={}".format(random_id))
+    # logging.info("random_id={}".format(random_id))
+    # print(f"got random id {random_id}")
 
     final = ""
-    fails = 0
-    total_fail = 0
+    fail = time.time()
+    lastTry = False
+    # total_fail = 0
     while "result_doc_url" not in final:
         logging.info("trying again... {}".format(final))
-        r = requests.post("https://instantview.telegram.org/api/{}".format(contest), cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=random_id))
+
+        r = requests.post("https://instantview.telegram.org/api/{}".format(contest), headers=headers, verify=verify, cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=random_id))
         final = r.json()
         random_id = final["random_id"]
 
         if "status" not in final:
-            # Get randomid again
-            r = requests.post("https://instantview.telegram.org/api/{}".format(contest), cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=""))
-            random_id = r.json()["random_id"]
-            logging.info("new random_id={}".format(random_id))
-            fails += 1
-            if fails >= 5:
-                logging.error("uhhh trying another hash maybe")
-                fails = 0
-                r = requests.get(d, cookies=cookies, params=dict(url=url))
-                hash = re.search("{}\\?hash=(.*?)\",".format(contest), str(r.content)).group(1)
-                logging.info("new hash={}".format(hash))
-                total_fail += 1
-                if total_fail >= 7:
-                    logging.error("naaah fuck this i'm out {}".format(url))
-                    return None
+            if "result_doc_url" not in final:
+                # print(time.time() - fail)
+                if time.time() - fail >= 5:
+                    if lastTry:
+                        print(f"struggling on page for more than 10 seconds, trying from start in 45s {url}")
+                        return None
+
+                    print(f"struggling on page for more than 5 seconds, trying without random_id {url}")
+                    random_id = ""
+                    lastTry = True
+                    fail = time.time()
+
+                # time.sleep(5)
+
+                # r = requests.get(d, cookies=cookies, params=dict(url=url), verify=verify)
+                # hash = re.search("{}\\?hash=(.*?)\",".format(contest), str(r.content)).group(1)
+                # logging.info("new hash={}".format(hash))
+
+                # r = requests.post("https://instantview.telegram.org/api/{}".format(contest), verify=verify, cookies=cookies, params=dict(hash=hash), data=dict(url=url, section=domain, method="processByRules", rules_id=templNumber, rules=rules, random_id=""))
+                # random_id = r.json()["random_id"]
+                # logging.info("new random_id={}".format(random_id))
+        else:
+            print(final)
+            # tree = etree.parse(final["status"], htmlparser)
+            # print("Status: " + tree.xpath("//text()")[0])
 
     random_id = final["random_id"]
     u = final["result_doc_url"]
+    preview_html = final["preview_html"]
 
     logging.info("loading page {}".format(u))
-    r = requests.get(u, cookies=cookies)
+    r = requests.get(u, verify=verify, cookies=cookies)
 
     if "NESTED_ELEMENT_NOT_SUPPORTED" in str(r.content):
         logging.error("NESTED_ELEMENT_NOT_SUPPORTED in {}".format(url))
 
-    htmlparser = etree.HTMLParser(remove_blank_text=True)
     tree = etree.parse(StringIO(str(r.content.decode("utf-8"))), htmlparser)
+    preview_html_tree = etree.parse(StringIO(preview_html), htmlparser)
 
     logging.info("-- FINISHED --")
-
-    return (d + "?url=" + url, tree)
+    return (d + "?url=" + url, tree, preview_html_tree)
 
 
 def compare(f, s):
     # You can remove elements before diff if you want to
-    #
+
     # for bad in s.xpath("//h6[@data-block=\"Kicker\"]"):
     #     bad.getparent().remove(bad)
-    #for bad in f.xpath("//footer[last()]"):
+    # for bad in f.xpath("//footer[last()]"):
     #    bad.getparent().remove(bad)
 
-    #for img in f.xpath("//img"):
+    # for img in f.xpath("//img"):
     #    del img.attrib["src"]
-    #for img in s.xpath("//img"):
+    # for img in s.xpath("//img"):
     #    del img.attrib["src"]
 
     pass
 
 
-def checkDiff(cookies, url, t1, t2):
+def checkDiff(nobrowser, cookies, url, t1, t2, browser=""):
+    # print("checkDiff")
     if not url.startswith("http"):
         url = "http://" + url
 
@@ -112,54 +146,61 @@ def checkDiff(cookies, url, t1, t2):
     if domain.startswith("www."):
         domain = domain[4:]
 
-    c = open(cookies, "r")
-    cl = c.read()
-    c.close()
+    f1 = None
+    s1 = None
 
-    cookie = SimpleCookie()
-    cookie.load(cl)
+    # TODO switch to next cookie if this fails too much
+    # For now just start over with the same cookie
+    cookies = [cookies]
+    cookie = -1
+    while f1 is None:
+        cookie += 1
+        if cookie >= len(cookies):
+            cookie = 0
+            time.sleep(45)
+        f1 = getHtml(domain, cookies[cookie], url, t1)
 
-    cookies = {}
-    for key, morsel in cookie.items():
-        cookies[key] = morsel.value
+    cookie = -1
+    while s1 is None:
+        cookie += 1
+        if cookie >= len(cookies):
+            cookie = 0
+            time.sleep(45)
+        s1 = getHtml(domain, cookies[cookie], url, t2)
 
-    f1 = getHtml(domain, cookies, url, t1)
-    s1 = getHtml(domain, cookies, url, t2)
     if f1 is None or s1 is None:
-        return "FAIL: " + url
+        return (url, -1)
     f = f1[1]
     s = s1[1]
+    preview_html_first = f1[2]
+    preview_html_second = s1[2]
 
     compare(f, s)
 
     a1 = f.xpath("//article")
+
     if len(a1) == 0:
         a1 = f.xpath("//section[@class=\"message\"]")
+        copy1 = copy.deepcopy(a1)
+    else:
+        copy1 = copy.deepcopy(a1)
+        for img in copy1[0].xpath("//img"):
+            del img.attrib["src"]
+        copy1[0].append(copy.deepcopy(preview_html_first).xpath("//div[@class='page-preview']")[0])
+
     a2 = s.xpath("//article")
     if len(a2) == 0:
         a2 = s.xpath("//section[@class=\"message\"]")
+        copy2 = copy.deepcopy(a2)
+    else:
+        copy2 = copy.deepcopy(a2)
+        for img in copy2[0].xpath("//img"):
+            del img.attrib["src"]
+        copy2[0].append(copy.deepcopy(preview_html_second).xpath("//div[@class='page-preview']")[0])
 
-    diff = difflib.HtmlDiff(wrapcolumn=90).make_file(etree.tostring(a1[0], pretty_print=True, encoding='UTF-8').decode("utf-8").split("\n"), etree.tostring(a2[0], pretty_print=True, encoding='UTF-8').decode("utf-8").split("\n"))
+    diff = difflib.HtmlDiff(wrapcolumn=90).make_file(etree.tostring(copy1[0], pretty_print=True, encoding='UTF-8').decode("utf-8").split("\n"), etree.tostring(copy2[0], pretty_print=True, encoding='UTF-8').decode("utf-8").split("\n"))
     htmlparser = etree.HTMLParser(remove_blank_text=True)
     tree = etree.parse(StringIO(str(diff)), htmlparser)
-
-    link1 = etree.Element("a", **{"href": f1[0]})
-    link1.text = "Template {}\n".format(t1)
-
-    link2 = etree.Element("a", **{"href": s1[0]})
-    link2.text = "Template {}".format(t2)
-
-    teleport = etree.Element("a", **{"href": "#flex"})
-    teleport.text = "go to frames"
-
-    tree.xpath("//body")[0].addprevious(teleport)
-    tree.xpath("//body")[0].addprevious(etree.Element("br"))
-    tree.xpath("//body")[0].addprevious(link1)
-    tree.xpath("//body")[0].addprevious(etree.Element("br"))
-    tree.xpath("//body")[0].addprevious(link2)
-    tree.xpath("//body")[0].addprevious(etree.Element("br"))
-    tree.xpath("//body")[0].addprevious(etree.Element("input", **{"value": url}))
-    tree.xpath("//body")[0].addprevious(etree.Element("br"))
 
     frame1_link = f.xpath("//head/link")
     frame1_link[0].attrib["href"] = "https://ivwebcontent.telegram.org{}".format(frame1_link[0].attrib["href"])
@@ -169,12 +210,29 @@ def checkDiff(cookies, url, t1, t2):
     tree.xpath("//head")[0].append(frame1_link[0])
     tree.xpath("//head")[0].append(frame1_script[0])
 
-    htmlparser = etree.HTMLParser(remove_blank_text=True)
-    append = etree.parse(open("append.html", "rb"), htmlparser)
+    htmlparser = etree.HTMLParser(remove_blank_text=True, encoding='utf-8')
+    append = etree.parse(open("append.html", "r", encoding='utf8'), htmlparser)
+
     frames = append.xpath("//div[contains(@id, 'frame')]")
     frames[0].append(a1[0])
     frames[1].append(a2[0])
-    tree.xpath("//body")[0].addnext(append.xpath("//div[contains(@class, 'flex')]")[0])
+
+    previews = append.xpath("//div[contains(@id, 'preview')]")
+    previews[0].append(preview_html_first.xpath("//div[@class='page-preview']")[0])
+    previews[1].append(preview_html_second.xpath("//div[@class='page-preview']")[0])
+
+    first_link = append.xpath("//a[@id='first_template']")[0]
+    first_link.attrib["href"] = f1[0]
+    first_link.text = "Template {}\n".format(t1)
+
+    second_link = append.xpath("//a[@id='second_template']")[0]
+    second_link.attrib["href"] = s1[0]
+    second_link.text = "Template {}\n".format(t2)
+
+    append.xpath("//input")[0].attrib["value"] = url
+
+    tree.xpath("//body//table")[0].addprevious(append.xpath("//main/div[1]")[0])
+    tree.xpath("//body//table")[0].addnext(append.xpath("//main/div[1]")[0])
 
     for bad in tree.xpath("//table[@summary='Legends']"):
         bad.getparent().remove(bad)
@@ -183,7 +241,6 @@ def checkDiff(cookies, url, t1, t2):
     # ДУМОТЬ ВСО ЕСЧО ВПАДЛУ
     # ХТО ЗАРЖАВ СТАВ РОФЛАН ЇБАЛО
     if "class=\"diff_add\"" in final or "class=\"diff_chg\"" in final or "class=\"diff_sub\"" in final:
-        print(f"diff {url}")
         md = md5()
         md.update(url.encode('utf-8'))
 
@@ -195,11 +252,26 @@ def checkDiff(cookies, url, t1, t2):
         file = open(fn, "w")
         file.write(final)
         file.close()
-        webbrowser.open_new_tab("file:///{}/{}".format(os.getcwd(), fn))
+        if not nobrowser:
+            browser = webbrowser if browser == "" else webbrowser.get(browser)
+            browser.open_new_tab("file:///{}/{}".format(os.getcwd(), fn))
+        return (url, 1)
     else:
-        print(f"no diff {url}")
+        return (url, 0)
 
-    return url
+
+def parseCookies(cookiesFile):
+    c = open(cookiesFile, "r")
+    cl = c.read()
+    c.close()
+
+    cookie = SimpleCookie()
+    cookie.load(cl)
+
+    cookies = {}
+    for key, morsel in cookie.items():
+        cookies[key] = morsel.value
+    return cookies
 
 
 if __name__ == "__main__":
@@ -208,7 +280,11 @@ if __name__ == "__main__":
     parser.add_argument('t2', metavar='second_template', type=str, help='second template number OR template file path')
     parser.add_argument('url', metavar='url', nargs='+', type=str, help='original page url to diff')
     parser.add_argument('--cookies', '-c', help='path to file with cookies (default is cookies.txt)', nargs='?', default="cookies.txt")
+    parser.add_argument('--nobrowser', '-n', help='do not open browser when diff is found', action='store_true')
+    parser.add_argument('--browser', '-b', help='browser or path to program to open diff', nargs='?', default="")
 
     args = parser.parse_args()
     for i in args.url:
-        checkDiff(args.cookies, i, args.t1, args.t2)
+        cookies = parseCookies(args.cookies)
+        for iz in range(0, 100):
+            checkDiff(args.nobrowser, cookies, i, args.t1, args.t2, args.browser)
--- spider.py
+++ spider.py
@@ -1,21 +1,25 @@
 import scrapy
-from ivdiff import checkDiff
+import ivdiff
 from urllib.parse import urlparse
 from multiprocessing import Pool
 from scrapy.crawler import CrawlerProcess
 import argparse
 from scrapy.utils.project import get_project_settings
 import os
-from urllib.parse import urljoin
+import json
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class IvSpider(scrapy.Spider):
+class IvSpider(scrapy.spiders.CrawlSpider):
     name = 'IvSpider'
     dupl = []
-    total = 0
+    url_list = {}
+    crawled = 0
     parsed = 0
+    have_diff = 0
 
-    def __init__(self, domain="", cookies="cookies.txt", poolsize=5, **kwargs):
+    def __init__(self, nobrowser=False, browser="", domain="", cookies="cookies.txt", poolsize=5, **kwargs):
         if not domain.startswith("http"):
             domain = "http://" + domain
         self.start_urls = [domain]
@@ -25,51 +29,56 @@ class IvSpider(scrapy.Spider):
             d = domain[4:]
 
         self.allowed_domains = [d]
+        self.browser = browser
+        self.nobrowser = nobrowser
 
         self.pool = Pool(poolsize)
+        self.rules = [Rule(LinkExtractor(allow=(), allow_domains=self.allowed_domains, deny=(), deny_domains=()), callback='parse_item', follow=True)]
 
-        self.cookies = cookies
-        fn = "gen/{}/parsed.txt".format(d)
+        self.cookies = ivdiff.parseCookies(cookies)
+        fn = "gen/{}/url_list.json".format(d)
         try:
             os.makedirs(os.path.dirname(fn))
         except Exception:
             pass
 
+        self.file = open(fn, "a+", buffering=1)
+        self.file.seek(0)
         try:
-            self.dupl = list(open(fn, "r"))
-        except Exception:
+            self.url_list = json.loads(self.file.read())
+        except Exception as ex:
+            print(ex)
             pass
-        self.dupl = list(map(lambda s: s.strip(), self.dupl))
-        print(self.dupl)
-        self.file = open(fn, "a")
+        self.file.seek(0)
+
         super().__init__(**kwargs)
 
     def callback(self, roflan):
         self.parsed += 1
-        print("{0:.2f}% [{1} / {2}] {3}".format(self.parsed / self.total * 100, self.parsed, self.total, roflan))
-        self.file.write("{}\n".format(roflan))
+        if roflan[1] == -1:
+            print(f"an error occured for url {roflan[0]}")
+            return
+        if roflan[1] == 1:
+            self.have_diff += 1
+        print("{0:.2f}% [{1} / crawled {2}] (w/ diff {3}) {4}".format(self.parsed / self.crawled * 100, self.parsed, self.crawled, self.have_diff, roflan[0]))
+        self.url_list[roflan[0]] = True
+        self.file.seek(0)
+        self.file.truncate(0)
+        self.file.seek(0)
+        self.file.write(json.dumps(self.url_list))
+        self.file.seek(0)
 
     def addToPool(self, url):
-        self.total += 1
-        self.pool.apply_async(checkDiff, [self.cookies, url, self.t1, self.t2], callback=self.callback)
-        pass
+        if url in self.url_list:
+            if not self.url_list[url]:
+                self.pool.apply_async(ivdiff.checkDiff, [self.nobrowser, self.cookies, url, self.t1, self.t2, self.browser], callback=self.callback)
+            return
+        self.crawled += 1
+        self.url_list[url] = False
+        self.pool.apply_async(ivdiff.checkDiff, [self.nobrowser, self.cookies, url, self.t1, self.t2, self.browser], callback=self.callback)
 
-    def parse(self, response):
-        for i in response.xpath("//a/@href"):
-            z = i.extract()
-            if z.endswith(".jpg") or z.endswith(".png") or z.endswith(".gif") or z.endswith(".jpeg"):
-                continue
-            z = urljoin(response.url, z.strip())
-            domain = urlparse(z).netloc
-            if domain.startswith("www."):
-                domain = domain[4:]
-            if not domain.startswith(self.allowed_domains[0]):
-                continue
-            yield response.follow(z, self.parse)
-            if z in self.dupl:
-                continue
-            self.dupl.append(z)
-            self.addToPool(z)
+    def parse_item(self, response):
+        self.addToPool(response.url)
 
 
 if __name__ == '__main__':
@@ -79,6 +88,8 @@ if __name__ == '__main__':
     parser.add_argument('domain', metavar='domain', type=str, help='domain to crawl')
     parser.add_argument('--cookies', '-c', help='path to file with cookies (default is cookies.txt)', nargs='?', default="cookies.txt")
     parser.add_argument('--poolsize', '-p', help='concurrent connections count(default=5)', type=int, nargs='?', default=5)
+    parser.add_argument('--nobrowser', '-n', help='do not open browser when diff is found', action='store_true')
+    parser.add_argument('--browser', '-b', help='browser or path to program to open diff', nargs='?', default="")
 
     args = parser.parse_args()
 
@@ -88,5 +99,5 @@ if __name__ == '__main__':
 
     process = CrawlerProcess(settings)
 
-    process.crawl(IvSpider, domain=args.domain, t1=args.t1, t2=args.t2, poolsize=args.poolsize)
+    process.crawl(IvSpider, nobrowser=args.nobrowser, browser=args.browser, domain=args.domain, t1=args.t1, t2=args.t2, poolsize=args.poolsize)
     process.start()
